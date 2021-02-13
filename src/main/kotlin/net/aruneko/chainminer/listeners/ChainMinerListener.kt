5fc84b9636e8d277253201024ce8b8aed15888fb
Aruneko <aruneko99@gmail.com>
2021-02-13 20:05:00 +0900

    remove unused argument

--- src/main/kotlin/net/aruneko/chainminer/listeners/ChainMinerListener.kt
+++ src/main/kotlin/net/aruneko/chainminer/listeners/ChainMinerListener.kt
@@ -3,6 +3,7 @@ package net.aruneko.chainminer.listeners
 import net.aruneko.chainminer.extensions.findVein
 import net.aruneko.chainminer.extensions.isOre
 import net.aruneko.chainminer.extensions.isPickaxe
+import net.aruneko.chainminer.extensions.sortByDistance
 import org.bukkit.Server
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
@@ -29,7 +30,7 @@ class ChainMinerListener(private val plugin: Plugin, private val server: Server)
             return
         }
 
-        val vein = block.findVein(listOf())
-        vein.forEach { it.breakNaturally(mainHandItem) }
+        val vein = block.findVein()
+        vein.sortByDistance(player).reversed().forEach { it.breakNaturally(mainHandItem) }
     }
 }
