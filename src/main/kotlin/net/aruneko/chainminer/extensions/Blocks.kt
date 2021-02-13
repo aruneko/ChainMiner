70ac777a1191e33cbd86d2d46f589e9890b0c344
Aruneko <aruneko99@gmail.com>
2021-02-13 20:04:37 +0900

    implement distance method

--- src/main/kotlin/net/aruneko/chainminer/extensions/Blocks.kt
+++ src/main/kotlin/net/aruneko/chainminer/extensions/Blocks.kt
@@ -1,7 +1,12 @@
 package net.aruneko.chainminer.extensions
 
 import org.bukkit.block.Block
+import org.bukkit.entity.Player
 
 fun List<Block>.extractEquals(block: Block) : List<Block> {
     return this.filter { it.type === block.type }
+}
+
+fun List<Block>.sortByDistance(player: Player): List<Block> {
+    return this.sortedBy { player.location.distance(it.location) }
 }
